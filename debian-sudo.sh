bca754c745b5a61981ec6fc5e613438f95b861a0
mhebbel2 <mark.hebbel@chainstep.com>
2025-01-10 12:21:31 +0100

    update

--- debian-sudo.sh
+++ debian-sudo.sh
@@ -20,11 +20,11 @@ echo \
 curl -fsSL https://deb.nodesource.com/setup_20.x -o nodesource_setup.sh
 bash nodesource_setup.sh >/dev/null
 #--- gh cli
-# curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg |  dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
-# && chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
-# && echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" |  tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg |  dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" |  tee /etc/apt/sources.list.d/github-cli.list > /dev/null
 #--- Now Install
-PACKAGES="git mosh tmux bash-completion ripgrep build-essential jq htop zip unzip fzf bat cmake nodejs rust"
+PACKAGES="git gc mosh tmux bash-completion ripgrep build-essential jq htop zip unzip fzf bat cmake nodejs rust"
 SPECIFIC_DEBIAN="python3-pip fd-find"
 DOCKER="uidmap docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin"
 
