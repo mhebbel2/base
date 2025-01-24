239dbf019113d95b5f2742e214d331ff4a381bd1
mhebbel2 <mark.hebbel@chainstep.com>
2025-01-24 18:28:22 +0100

    update

--- debian-sudo.sh
+++ debian-sudo.sh
@@ -16,15 +16,8 @@ echo \
    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
 	$(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
 	tee /etc/apt/sources.list.d/docker.list > /dev/null
-#--- node 
-# curl -fsSL https://deb.nodesource.com/setup_20.x -o nodesource_setup.sh
-# bash nodesource_setup.sh >/dev/null
-#--- gh cli
-# curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg |  dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
-# && chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
-# && echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" |  tee /etc/apt/sources.list.d/github-cli.list > /dev/null
 #--- Now Install
-PACKAGES="git mosh tmux bash-completion ripgrep build-essential jq htop zip unzip fzf bat cmake "
+PACKAGES="git mosh tmux bash-completion ripgrep build-essential jq htop zip unzip bat cmake "
 SPECIFIC_DEBIAN="python3-pip fd-find"
 SPECIFIC_AIC="sqlite3"
 DOCKER="uidmap docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin"
