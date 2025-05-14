19154b587669cc52046932b106521e41ea3e126b
mhebbel2 <mark.hebbel@chainstep.com>
2025-05-14 18:03:10 +0200

    update

--- debian-sudo.sh
+++ debian-sudo.sh
@@ -10,7 +10,7 @@ echo \
 	$(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
 	tee /etc/apt/sources.list.d/docker.list > /dev/null
 #--- Now Install
-PACKAGES="git tmux bash-completion ripgrep build-essential jq htop zip unzip bat cmake yq"
+PACKAGES="git tmux bash-completion ripgrep build-essential jq htop zip unzip bat cmake "
 VNC="tigervnc-standalone-server tigervnc-common tigervnc-tools xfce4 xfce4-terminal firefox-esr ufw"
 SPECIFIC_DEBIAN="python3-pip fd-find"
 DOCKER="uidmap docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin"
@@ -18,7 +18,6 @@ SPECIFIC_NVIM="fuse libfuse2"
 
 apt-get -y -qq update
 apt-get -y -qq install $PACKAGES $VNC $SPECIFIC_DEBIAN $SPECIFIC_NVIM $DOCKER >/dev/null
-
 # ---
 sed -i '/^AcceptEnv/s/$/ *_TOKEN *_API_KEY/' /etc/ssh/sshd_config
 sed -i 's/#AllowAgentForwarding yes/AllowAgentForwarding yes/g' /etc/ssh/sshd_config && systemctl restart sshd
