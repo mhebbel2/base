e5ffa7d49dde590ec38c1f4a3d7fcb764c32a39a
mhebbel2 <mark.hebbel@chainstep.com>
2025-08-29 15:01:21 +0200

    update

--- debian-user.sh
+++ debian-user.sh
@@ -14,21 +14,6 @@ get_machine_type() {
 }
 
 
-get_machine_type_nvim() {
-    local machine_type=$(uname -m)
-    case "$machine_type" in
-        x86_64)
-            echo "x86_64"
-            ;;
-        aarch64)
-            echo "arm64"
-            ;;
-        *)
-            echo "Unknown architecture"
-            ;;
-    esac
-}
-
 
 
 BINDIR=$HOME/.local/bin
@@ -40,19 +25,11 @@ mkdir -p $TMPDIR
 ln -sf /usr/bin/batcat $HOME/.local/bin/bat
 ln -sf /usr/bin/fdfind $HOME/.local/bin/fd
 
-# nvim (until debian-13)
-curl -L https://github.com/neovim/neovim/releases/download/stable/nvim-linux-$(get_machine_type_nvim).appimage -o $HOME/.local/bin/nvim
-chmod u+x $HOME/.local/bin/nvim
-
 MTYPE=$(get_machine_type)
 
 
 # --- get the right yq for debian (the standard one is strange) 
 curl -L https://github.com/mikefarah/yq/releases/download/v4.45.4/yq_linux_$MTYPE -O $BINDIR/yq && chmod +x $BINDIR/yq
-
-# fzf (until debian-13)
-curl -L https://github.com/junegunn/fzf/releases/download/v0.61.1/fzf-0.61.1-linux_$MTYPE.tar.gz -o $TMPDIR/fzf.tgz
-tar -xf $TMPDIR/fzf.tgz -C $BINDIR fzf
 
 # gh cli
 curl -L https://github.com/cli/cli/releases/download/v2.70.0/gh_2.70.0_linux_$MTYPE.tar.gz -o $TMPDIR/gh.tgz
@@ -78,4 +55,3 @@ tar -xzf $TMPDIR/flux.tgz -C $BINDIR
 
 # hcloud
 curl -sSL https://github.com/hetznercloud/cli/releases/download/v1.50.0/hcloud-linux-$MTYPE.tar.gz -o $TMPDIR/hcloud.tgz
-tar -xzf $TMPDIR/hcloud.tgz -C $BINDIR hcloud
