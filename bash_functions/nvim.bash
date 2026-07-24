e8879d838f207a6a4cc2233dfaad170b0ae0356c
mhebbel2 <mark.hebbel@chainstep.com>
2026-07-24 15:11:08 +0200

    update

--- bash_functions/nvim.bash
+++
@@ -1,9 +0,0 @@
-# v() {
-#   if [ -n "$NVIM" ]; then
-#     # We are inside an nvim terminal
-#     nvim --server "$NVIM" --remote "$@"
-#   else
-#     # We are in a standard terminal
-#     nvim "$@"
-#   fi
-# }
--- debian-sudo.sh
+++ debian-sudo.sh
@@ -2,25 +2,15 @@
 #
 export DEBIAN_FRONTEND=noninteractive
 
-#--- Docker
-curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
-chmod a+r /etc/apt/keyrings/docker.asc
-echo \
-   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
-	$(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-	tee /etc/apt/sources.list.d/docker.list > /dev/null
-
 #--- Now Install
 # Use --no-install-recommends to skip optional bloat
 INSTALL_OPTS="-y -qq --no-install-recommends"
 
 PACKAGES="git dtach fzf ufw wireguard bash-completion ripgrep fd-find jq python3-pip python3.13-venv pipx rclone keychain keepassxc-minimal build-essential lsof sshfs"
-DOCKER="uidmap docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin"
-WAYLAND="sway wayvnc"
 RDP_DESKTOP="xfce4 xfce4-goodies xrdp chromium copyq"
 
 apt-get $INSTALL_OPTS update
-apt-get $INSTALL_OPTS install $PACKAGES $DOCKER
+apt-get $INSTALL_OPTS install $PACKAGES
 apt-get $INSTALL_OPTS install $RDP_DESKTOP
 
 # ---
