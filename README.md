a51061d05f3767894c826467716a8f3e57e59c74
mhebbel2 <mark.hebbel@chainstep.com>
2026-07-24 15:45:16 +0200

    update

--- README.md
+++ README.md
@@ -8,6 +8,8 @@ time ./base/debian-sudo.sh
 
 # termux
 
+
+Classic:
 ```
 termux-setup-storage
 pkg update -qq && pkg upgrade -y -qq
@@ -15,4 +17,10 @@ pkg install -yy openssh git
 pkg install bash-completion neovim fzf keychain termux-services x11-repo keepassxc
 git clone https://github.com/mhebbel2/base.git
 ```
+
+core-termux:
+```
+curl -fsSL https://raw.githubusercontent.com/DevCoreXOfficial/core-termux/main/install.sh | bash
+```
+
 
--- bash_functions/git.bash
+++ bash_functions/git.bash
@@ -5,21 +5,11 @@ function branch() {
 }
 export -f branch
 
-function clone_repo() {
-	local repo=$1
-	git -C $PROJECTS clone $repo
-	cd $PROJECTS/$(basename $repo .git)
-	pwd
-}
-
-# function codeberg() {
-# 	local repo=$(curl -sS -H "Authorization: token $CODEBERG_API_TOKEN"  "https://codeberg.org/api/v1/user/repos" | jq -r '.[] | "\(.ssh_url)"' | fzf)
-# 	clone_repo "$repo"
-# }
-#
 function github() {
 	# local repo=$(gh repo list --no-archived --json sshUrl --jq '.[].sshUrl' -L 400 $1 |fzf)
 	local repo=$(cat $HOME/.local/github_repos.txt | fzf)
-	clone_repo "$repo"
-	$PROJECTS/work/bin/set_git_config.sh
+	git -C $PROJECTS clone $repo
+	cd $PROJECTS/$(basename $repo .git)
+	pwd
+	$PROJECTS/base/bin/set_git_config.sh
 }
