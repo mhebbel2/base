c658f5e7a6d8486a07bb2a1df717a8fd9f9b4d36
mhebbel2 <mark.hebbel@chainstep.com>
2026-08-07 11:13:51 +0200

    update

--- bin/start_debian.sh
+++
@@ -1,1 +0,0 @@
-proot-distro login debian -u user --bind /data/data/com.termux/files/home/storage:/home/user/storage --bind /data/data/com.termux/files/home/projects:/home/user/projects
--- bin/start_miniserve.sh
+++
@@ -1,2 +0,0 @@
-nohup miniserve --interfaces 10.99.0.1 $PROJECTS >& $HOME/serve.log &
-
