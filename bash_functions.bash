1c93e19adb5c96ff0124e8c0085f532a99e146d8
mhebbel2 <mark.hebbel@chainstep.com>
2026-08-07 11:20:55 +0200

    update

--- bash_functions.bash
+++ bash_functions.bash
@@ -1,3 +1,23 @@
+function branch() {
+	BRANCHNAME=$1
+	git checkout -b $BRANCHNAME
+	git push origin $BRANCHNAME --set-upstream
+}
+export -f branch
+
+function github() {
+	# local repo=$(gh repo list --no-archived --json sshUrl --jq '.[].sshUrl' -L 400 $1 |fzf)
+	local repo=$(cat $HOME/.local/github_repos.txt | fzf)
+	git -C $PROJECTS clone $repo
+	cd $PROJECTS/$(basename $repo .git)
+	pwd
+	$PROJECTS/base/bin/set_git_config.sh
+}
+
+ssh-load() {
+    keepassxc-cli show -s -a "$1" "$KEEPASS_DB" sshkeys | ssh-add -
+}
+
 function kc() {
 	if [ -z "$1" ]; then
 	  QUERY=""
--- bash_functions/keepass.bash
+++
@@ -1,4 +0,0 @@
-ssh-load() {
-    keepassxc-cli show -s -a "$1" "$KEEPASS_DB" sshkeys | ssh-add -
-    # keepassxc-cli attachment-export "$KEEPASS_DB" "sshkeys" "$1" --stdout | ssh-add -
-}
