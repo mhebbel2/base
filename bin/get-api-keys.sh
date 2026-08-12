e80be0eeffe7fd05d88baf12d7736174e3c6fb7a
mhebbel2 <mark.hebbel@chainstep.com>
2026-08-12 10:25:52 +0200

    update

--- bin/get-api-keys.sh
+++ bin/get-api-keys.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-DB=$KEEPASS_DB
 TOKENS_FILE=$HOME/.local/tokens.sh
 
-if [ ! -f "$DB" ]; then
-    echo "Error: Database file '$DB' not found."
+if [ ! -f "$KEEPASS_DB" ]; then
+    echo "Error: Database file '$KEEPASS_DB' not found."
     exit 1
 fi
 
@@ -16,12 +15,12 @@ if [ -f "$TOKENS_FILE" ]; then
 fi
 touch $TOKENS_FILE
 
-SEARCH_RESULTS=$(echo $KEEPASS_XC_PASS | keepassxc-cli search -q "$DB" "+attr:API_KEY")
+SEARCH_RESULTS=$(echo $KEEPASS_XC_PASS | keepassxc-cli search -q "$KEEPASS_DB" "+attr:API_KEY")
 
 echo "$SEARCH_RESULTS" | while read -r entry_path; do
 	echo $entry_path
-    NAME=$(echo $KEEPASS_XC_PASS | keepassxc-cli show -q  -s -a "API_KEY_NAME" "$DB" "$entry_path")
-    KEY=$(echo $KEEPASS_XC_PASS | keepassxc-cli show -q  -s -a "API_KEY" "$DB" "$entry_path")
+    NAME=$(echo $KEEPASS_XC_PASS | keepassxc-cli show -q  -s -a "API_KEY_NAME" "$KEEPASS_DB" "$entry_path")
+    KEY=$(echo $KEEPASS_XC_PASS | keepassxc-cli show -q  -s -a "API_KEY" "$KEEPASS_DB" "$entry_path")
 
     # Sanitize NAME for shell compatibility (replace spaces/special chars with _)
     SAFE_NAME=$(echo "$NAME" | sed 's/[^a-zA-Z0-9_]/_/g')
